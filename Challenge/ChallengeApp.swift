5d1925415499e9081e7fedacd7dd94dcef1dfb9e
Lynk <alanjholguin@gmail.com>
2021-01-29 17:11:33 -0500

    Initial Commit

---
+++ Challenge/ChallengeApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by Lynk on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
